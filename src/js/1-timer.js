b1716cccee73e2e84a300cb939122718ccc28a12
Svitlana Kyryk <kyryk.svitlana@gmail.com>
2024-06-11 22:55:58 +0200

    up

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -1,7 +1,9 @@
 import flatpickr from "flatpickr";
 import "flatpickr/dist/flatpickr.min.css";
+
 import iziToast from "izitoast";
 import "izitoast/dist/css/iziToast.min.css";
+
 const datetimePicker = document.querySelector("#datetime-picker");
 const startBtn = document.querySelector("[data-start]");
 const daysEl = document.querySelector("[data-days]");
@@ -10,6 +12,7 @@ const minutEl = document.querySelector("[data-minutes]");
 const secondsEl = document.querySelector("[data-seconds]");
 let timerInterval;
 let userSelectedDate;
+
 const options = {
 enableTime: true,
 time_24hr: true,
@@ -29,12 +32,11 @@ startBtn.disabled = false;
 }
 },
 };
-
 flatpickr(datetimePicker, options);
 startBtn.addEventListener("click", () => {
-    startBtn.disabled = true;
-    datetimePicker.disabled = true;
-    startCountdown();
+startBtn.disabled = true;
+datetimePicker.disabled = true;
+startCountdown();
 });
 
 function startCountdown() {
@@ -45,6 +47,7 @@ if (timeRemaining <= 0) {
 clearInterval(timerInterval);
 updateTimerDisplay(0, 0, 0, 0);
 datetimePicker.disabled = false;
+startBtn.disabled = false;
 return;
 }
 const { days, hours, minutes, seconds } = convertMs(timeRemaining);
@@ -52,26 +55,22 @@ updateTimerDisplay(days, hours, minutes, seconds);
 }, 1000);
 }
 function convertMs(ms) {
-    const second = 1000;
-    const minute = second * 60;
-    const hour = minute * 60;
-    const day = hour * 24;
-
-    const days = Math.floor(ms / day);
-    const hours = Math.floor((ms % day) / hour);
-    const minutes = Math.floor(((ms % day) % hour) / minute);
-    const seconds = Math.floor((((ms % day) % hour) % minute) / second);
-
-    return { days, hours, minutes, seconds };
+const second = 1000;
+const minute = second * 60;
+const hour = minute * 60;
+const day = hour * 24;
+const days = Math.floor(ms / day);
+const hours = Math.floor((ms % day) / hour);
+const minutes = Math.floor(((ms % day) % hour) / minute);
+const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+return { days, hours, minutes, seconds };
 }
-
 function updateTimerDisplay(days, hours, minutes, seconds) {
-    daysEl.textContent = addLeadingZero(days);
-    hoursEl.textContent = addLeadingZero(hours);
-    minutEl.textContent = addLeadingZero(minutes);
-    secondsEl.textContent = addLeadingZero(seconds);
+daysEl.textContent = addLeadingZero(days);
+hoursEl.textContent = addLeadingZero(hours);
+minutEl.textContent = addLeadingZero(minutes);
+secondsEl.textContent = addLeadingZero(seconds);
 }
-
 function addLeadingZero(value) {
-    return String(value).padStart(2, '0');
+return String(value).padStart(2, '0');
 }
