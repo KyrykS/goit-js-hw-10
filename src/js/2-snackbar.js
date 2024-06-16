659d3ec309e5a269b1e8acc05edd776a0276e0c9
Svitlana Kyryk <kyryk.svitlana@gmail.com>
2024-06-16 12:47:05 +0200

    up

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -1,33 +1,33 @@
 document.getElementById('promiseForm').addEventListener('submit', function(event) {
-    event.preventDefault();
+event.preventDefault();
   
-    const delay = parseInt(event.target.delay.value);
-    const state = event.target.state.value;
+const delay = parseInt(event.target.delay.value);
+const state = event.target.state.value;
   
-    const promise = new Promise((resolve, reject) => {
-      setTimeout(() => {
-        if (state === 'fulfilled') {
-          resolve(delay);
-        } else {
-          reject(delay);
-        }
-      }, delay);
-    });
+const promise = new Promise((resolve, reject) => {
+setTimeout(() => {
+if (state === 'fulfilled') {
+resolve(delay);
+} else {
+reject(delay);
+}
+}, delay);
+});
   
-    promise
-      .then((delay) => {
-        iziToast.success({
-          title: 'Success',
-          message: `✅ Fulfilled promise in ${delay}ms`,
-          position: 'topRight',
-        });
-      })
-      .catch((delay) => {
-        iziToast.error({
-          title: 'Error',
-          message: `❌ Rejected promise in ${delay}ms`,
-          position: 'topRight',
-        });
-      });
+promise
+.then((delay) => {
+iziToast.success({
+title: 'Success',
+message: `✅ Fulfilled promise in ${delay}ms`,
+position: 'topRight',
+});
+})
+.catch((delay) => {
+iziToast.error({
+title: 'Error',
+message: `❌ Rejected promise in ${delay}ms`,
+position: 'topRight',
+});
+});
   });
   
